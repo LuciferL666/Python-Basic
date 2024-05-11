7c0bcacad7d350828dcb0abfaf24b0d61d3db042
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-11 09:56:15 +0200

    Create Projects-Creation.py

---
+++ 01-Lection/Projects-Creation.py
@@ -0,0 +1,5 @@
+name = input()
+projects = int(input())
+one_project_time = 3
+time_need = projects * one_project_time
+print(f"The architect {name} will need {time_need} hours to complete {projects} project/s.")

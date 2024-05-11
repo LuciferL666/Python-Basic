ef465ec199af2b6266526ec2611d7339ebd9bc7d
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-11 09:56:11 +0200

    Create Pet-Shop.py

---
+++ 01-Lection/Pet-Shop.py
@@ -0,0 +1,8 @@
+dog_food = int(input())
+cat_food = int(input())
+dog_food_price = 2.50
+cat_food_price = 4.00
+total_dog_food_price = dog_food * dog_food_price
+total_cat_food_price = cat_food * cat_food_price
+total_price = total_dog_food_price + total_cat_food_price
+print(f"{total_price} lv.")

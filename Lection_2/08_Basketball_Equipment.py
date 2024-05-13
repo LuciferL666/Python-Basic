7e5a6bc0304b5d7195839761a33efe33eb465a00
PeykoGeorgiev <peyko92gm@gmail.com>
2024-05-13 13:39:39 +0200

    Create 08_Basketball_Equipment.py

---
+++ Lection_2/08_Basketball_Equipment.py
@@ -0,0 +1,12 @@
+year_tax = int(input())
+
+basketball_shoes = year_tax * 0.6
+
+basketball_clothes = basketball_shoes * 0.8
+
+basketball_ball_price = basketball_clothes / 4
+basketball_accessoires_price = basketball_ball_price / 5
+
+total_money = year_tax + basketball_shoes + basketball_clothes + basketball_ball_price + basketball_accessoires_price
+
+print(total_money)
